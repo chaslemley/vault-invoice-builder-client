364a3dcd3a44000228e9ee6c8445b993e5a07f9a
Chris Continanza <christopher.continanza@gmail.com>
2013-05-09 16:21:56 -0700

    version bump

--- lib/vault-invoice-builder-client/version.rb
+++ lib/vault-invoice-builder-client/version.rb
@@ -2,7 +2,7 @@ module Vault
   module InvoiceBuilder
     class Client
       # The `Vault::InvoiceBuilder::Client` gem version.
-      VERSION = '0.0.1'
+      VERSION = '0.0.2'
     end
   end
 end
